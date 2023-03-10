1e7a7ffece9c3e150f8ff93d15a59d3105dbac85
Kartikkala <kartikkala10december@gmail.com>
2023-03-10 20:20:37 +0530

    Test commit

--- first_program.js
+++ first_program.js
@@ -1,1 +1,11 @@
-console.log("Hii");
+const http = require('node:http');
+
+function myFunction(req, res) {
+    res.writeHead(200, { 'Content-type': "text/html" });
+    res.write("Hemlo");
+    res.end();
+}
+const server = http.createServer(myFunction);
+
+
+server.listen(80, '0.0.0.0', () => { console.log("Hehe") });
