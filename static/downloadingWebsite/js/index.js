e830fc2614667a608e2f6ad11facd3b7895c7a1a
Kartikkala <kartikkala10december@gmail.com>
2023-04-25 13:07:34 +0530

    Updated website design

--- static/downloadingWebsite/js/index.js
+++ static/downloadingWebsite/js/index.js
@@ -1,25 +1,102 @@
 const xhr = new XMLHttpRequest();
 let downloadList = [];
 
-xhr.open("GET", "/downloads");
+function addTextLink(url, linkContent, className, elementId)
+{
+    let newLink = document.createElement("a");
+    newLink.href = url;
+    newLink.appendChild(document.createTextNode(linkContent));
+    if(className!=undefined)
+    {
+        newLink.setAttribute('class', className);
+    }
+    if(elementId !=undefined)
+    {
+        newLink.setAttribute('id', elementId);
+    }
+    return newLink;
+}
 
-console.log(xhr.readyState);
+function addDivision(className, elementId)
+{
+    let newElement = document.createElement("div");
+    if(className !=undefined)
+    {
+        newElement.setAttribute('class', className);
+    }
+    if(elementId!=undefined)
+    {
+        newElement.setAttribute('id', elementId);
+    }
+    return newElement;
+}
+
+function addImage(url, className, elementId)
+{
+    let image = document.createElement("img");
+    image.src = url;
+    if(className !=undefined)
+    {
+        image.setAttribute('class', className);
+    }
+    if(elementId!=undefined)
+    {
+        image.setAttribute('id', elementId);
+    }
+    return image;
+}
+
+function addImageWithURL(imageUrl, redirectionUrl, imageClassName, redirectionUrlClassName, imageElementId, redirectionUrlElementId)
+{
+    let link = document.createElement("a");
+    link.href = redirectionUrl;
+    let image = document.createElement("img");
+    image.src = imageUrl;
+    if(imageClassName !=undefined)
+    {
+        image.setAttribute('class', imageClassName);
+    }
+    if(imageElementId!=undefined)
+    {
+        image.setAttribute('id', imageElementId);
+    }
+    if(redirectionUrlClassName !=undefined)
+    {
+        link.setAttribute('class', redirectionUrlClassName);
+    }
+    if(redirectionUrlElementId!=undefined)
+    {
+        link.setAttribute('id', redirectionUrlElementId);
+    }
+    link.appendChild(image);
+    return link;
+}
+
+function addFileObject(listItem)
+{
+    let downloadUrl = "downloadFile" + "/" + listItem;
+    let newFileItem = addDivision('fileItems');
+    let fileActions = addDivision('actionBar');
+
+    let buttonIcon = addImageWithURL("../assets/icons/download.png", downloadUrl, 'actionButton');
+    let newLink = addTextLink(downloadUrl, listItem, 'bodyText');
+
+    fileActions.appendChild(buttonIcon);
+    newFileItem.appendChild(newLink);
+    newFileItem.append(fileActions);
+
+    const existingDiv = document.getElementById("primary");
+    existingDiv.appendChild(newFileItem);
+}
+
+xhr.open("GET", "/downloads");
 xhr.send()
-console.log(xhr.readyState);
 xhr.onload = () => {
     downloadList = xhr.response;
     downloadList = JSON.parse(downloadList);
 
     for (let i = 0; i < downloadList.length; i++) {
-
-        let newLink = document.createElement("a");
-        let linkContent = document.createTextNode(downloadList[i]);
-
-        newLink.appendChild(linkContent);
-        newLink.href = "downloadFile" + "/" + downloadList[i];
-
-        const existingDiv = document.getElementById("primary");
-        existingDiv.appendChild(newLink);
+        addFileObject(downloadList[i]);
     }
 }
 
