d6b4d863092ccb3d3693435596e7af301342b694
Kartikkala <kartikkala10december@gmail.com>
2023-08-11 10:17:21 +0530

    Implemented custom download function that uses streams to download

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const fs = require("fs");
+const util = require('util')
+const {Throttle} = require('stream-throttle')
+
+const statAsync = util.promisify(fs.stat)
+
 const path = require('path');
 const app = express();
 
@@ -8,6 +13,7 @@ app.disable('x-powered-by');
 const downloadDir = 'downloadables';
 const downloadSiteName = path.join(__dirname, "/static/", "/downloadingWebsite/");
 const targetVolume = path.join(__dirname, downloadDir);
+
 
 class fileManager{
     #targetPath = undefined;
@@ -146,6 +152,17 @@ class fileManager{
     }
 }
 
+function downloadFile(response, filePath, suggestedFileName, size)
+{
+    const throttle = new Throttle({rate: 2e+7})
+    const readableStream = fs.createReadStream(filePath, {'highWaterMark': 120000})
+    let headers = {'Content-Type':'application/octet-stream', 'Content-Disposition':`attachment;filename="${suggestedFileName}"`,'Content-Length':size}
+    response.writeHead(200, headers)
+    readableStream.pipe(throttle).pipe(response)
+    readableStream.on('close', ()=>console.log(`File ${filePath} has been downloaded`))
+}
+
+
 const fileObject = new fileManager;
 fileObject.setTargetPath(targetVolume);
 
@@ -164,10 +181,13 @@ app.get("/downloads", (request, response) => {
 
 
 
-app.get("/downloadFile/:filename", (request, response) => {
-    filename = request.params.filename;
-    let filePath = path.join(targetVolume, filename);
-    response.download(filePath);
+app.get("/downloadFile/:filename", async (request, response) => {
+    filename = request.params.filename
+    let filePath = path.join(targetVolume, filename)
+    let fileStats = await statAsync(filePath)
+    let fileSize = fileStats.size
+    console.log(fileSize)
+    downloadFile(response, filePath, request.params.filename, fileSize)
 })
 
 
