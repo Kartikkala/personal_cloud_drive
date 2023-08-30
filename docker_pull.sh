f233a06a8d9ff8e8be2c5961d1829fcb8f1fa8f2
Kartikkala <kartikkala10december@gmail.com>
2023-08-30 13:04:26 +0530

    Fixed docker_pull shell script

--- docker_pull.sh
+++ docker_pull.sh
@@ -2,5 +2,5 @@
 
 sshpass -P passphrase -p $SSH_PASS ssh $SSH_USR@$SSH_SERVER_ADDRESS -i $SSH_PRIVKEY docker pull kartikkala/mirror_website:$VERSION
 sshpass -P passphrase -p $SSH_PASS ssh $SSH_USR@$SSH_SERVER_ADDRESS -i $SSH_PRIVKEY CONTAINER_ID=$(docker ps -q --filter "ancestor=kartikkala/mirror_website")
-sshpass -P passphrase -p $SSH_PASS ssh $SSH_USR@$SSH_SERVER_ADDRESS -i $SSH_PRIVKEY docker kill CONTAINER_ID
+sshpass -P passphrase -p $SSH_PASS ssh $SSH_USR@$SSH_SERVER_ADDRESS -i $SSH_PRIVKEY docker kill $CONTAINER_ID
 sshpass -P passphrase -p $SSH_PASS ssh $SSH_USR@$SSH_SERVER_ADDRESS -i $SSH_PRIVKEY docker run -d kartikkala/mirror_website:$VERSION
