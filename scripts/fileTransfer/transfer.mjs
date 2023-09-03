bd1e93824b390cc7eefc16ef14f4fcffb33930b4
Kartikkala <kartikkala10december@gmail.com>
2023-09-03 17:12:56 +0530

    Added try catch blocks in downloadFileClient function from scripts/fileTransfer/transfer.mjs

--- scripts/fileTransfer/transfer.mjs
+++ scripts/fileTransfer/transfer.mjs
@@ -15,16 +15,23 @@ export class FileTransfer
     }
     async downloadFileClient(response, filePath, suggestedFileName)
     {
-        console.log("File path is: "+filePath)
-        let fileStats = await statAsync(filePath)
-        let fileSize = fileStats.size
-        console.log("File size in bytes: "+fileSize)
-        const throttle = new Throttle({rate: 8e+7})
-        const readableStream = fs.createReadStream(filePath, {'highWaterMark': 4e+7})
-        let headers = {'Content-Type':'application/octet-stream', 'Content-Disposition':`attachment;filename="${suggestedFileName}"`,'Content-Length':fileSize}
-        response.writeHead(200, headers)
-        readableStream.pipe(throttle).pipe(response)
-        readableStream.on('close', ()=>console.log(`File ${filePath} has been downloaded`))
-        return
+        let fileStats = undefined
+        try{
+            fileStats = await statAsync(filePath)
+            console.log("File size in bytes: "+fileStats.size)
+            const throttle = new Throttle({rate: 8e+7})
+            const readableStream = fs.createReadStream(filePath, {'highWaterMark': 4e+7})
+            let headers = {'Content-Type':'application/octet-stream', 'Content-Disposition':`attachment;filename="${suggestedFileName}"`,'Content-Length':fileStats.size}
+            response.writeHead(200, headers)
+            readableStream.pipe(throttle).pipe(response)
+            readableStream.on('close', ()=>console.log(`File ${filePath} has been downloaded`))
+            return
+        }
+        catch(err){
+            if(err){
+                response.status(404).send("Error, requested resource not found!!!")
+                return
+            }
+        }
     }
 }
