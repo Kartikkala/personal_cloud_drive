f0c3dc8b01b51771f09e68e1d69814e45464e23a
Kartikkala <kartikkala10december@gmail.com>
2024-02-28 20:41:37 +0530

    Moved download status update logic from socketioMiddleware to aria2Helper class

--- configs/app_config.js
+++ configs/app_config.js
@@ -5,6 +5,5 @@ const user_auth_db_configs = configData.user_auth_db_configs
 const session_configs = configData.session_configs
 const file_manager_configs = configData.fileManagerConfigs
 const keys_configs = configData.keys_configuration
-const socketio_configs = configData.socketioConfigs
 
-export { aria2_configs, user_auth_db_configs, session_configs, file_manager_configs, keys_configs, socketio_configs}
+export { aria2_configs, user_auth_db_configs, session_configs, file_manager_configs, keys_configs}
