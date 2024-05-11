b5a5e68d1721d1b2098ebdf9234226f73deb018c
Kartikkala <kartikkala10december@gmail.com>
2024-05-11 10:40:10 +0530

    Updated configuration files according to latest changes

--- configs/app_config.js
+++ configs/app_config.js
@@ -1,9 +1,8 @@
 import configData from "./app_config.json" assert {type : "json"}
 
 const aria2_configs = configData.aria2_configs
-const user_auth_db_configs = configData.user_auth_db_configs
-const session_configs = configData.session_configs
+const db_configs = configData.db_configs
 const app_configs = configData.app_configs
 const keys_configs = configData.keys_configuration
 
-export { aria2_configs, user_auth_db_configs, session_configs, app_configs, keys_configs}
+export { aria2_configs, db_configs, app_configs, keys_configs}
