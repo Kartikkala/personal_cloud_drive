ced5b7390f91f603a29549ee8e795491264bb36c
Kartikkala <kartikkala10december@gmail.com>
2024-06-07 12:13:34 +0530

    tsconfig, start.sh changes with web-torrent module version upgraded

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --disable-ipv6 &
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --disable-ipv6 --bt-metadata-only=true --bt-save-metadata=true &
 
 npm start
 
