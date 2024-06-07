cf8f3f0753ea4aa2a1847e575f1b08bd9f343745
Kartikkala <kartikkala10december@gmail.com>
2024-06-07 12:12:35 +0530

    Added null type support for stream in videoStreamResult

--- types/lib/http-streaming/types.ts
+++ types/lib/http-streaming/types.ts
@@ -1,7 +1,7 @@
 import { Readable } from "stream"
 
 export interface IVideoStreamResult{
-    stream : Readable | undefined,
+    stream : Readable | undefined | null,
     size : number | undefined,
     start : number | undefined,
     end : number | undefined
